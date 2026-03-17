6d624cea3f36aaf5d491fc30a846d30142e37802
CSEKU230220-II <147187748+23kukazicse@users.noreply.github.com>
2026-03-17 16:13:05 +0600

    asdfdasf

--- DynamicMemoryAllocation/malloc.c
+++ DynamicMemoryAllocation/malloc.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main(void) {
   int *p;
   // p = (int *)malloc(100 * sizeof(int));
   // if (p == NULL){
   if ((p = (int *)malloc(10 * sizeof(int))) == NULL) { // WORKING
     printf("NO MEMORY WAS ALLOCATED, hence, no space available in your "
-           "memory(RAM)\nExiting...");
+           "memory(RAM) \n\nExiting...");
     exit(1);
   } else {
+	  // hello
     printf("Memory allocation Successful!");
   }
   p = realloc(p, 100 * sizeof(int));
